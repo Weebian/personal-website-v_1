5db5bb5c35f9248bad2e987efaf912323d2e2084
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-09-08 18:54:18 -0400

    Move stuff

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 $(function () {
-
+    let active = false;
+    
     //Menu stuff
     $(".menuButton").click(function () {
         if ($('.menuItems').is(':hidden')) {
@@ -52,5 +53,21 @@ $(function () {
                     });
                 }
             }
+        });
+        $(function () {
+            $(".accordion").accordion();
+        });
+        $('.accordion').accordion({
+            active: false,
+            collapsible: true,
+            heightStyle: "content",
+        });
+    
+        $(".accordion").click(function () {
+            console.log(active);
+            $("#folder" + active).switchClass("fa-folder-open", "fa-folder");
+            active = $(".accordion").accordion("option", "active");
+            console.log(active);
+            $("#folder" + active).switchClass("fa-folder", "fa-folder-open");
         });
 });

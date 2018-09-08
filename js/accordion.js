ccca5ce1ca92ce3fe74b46cf1f6b9c7e0f6f1293
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-09-08 18:57:44 -0400

    seperate js file

--- js/accordion.js
+++ js/accordion.js
@@ -1,4 +1,5 @@
 $(function () {
+    let active = false;
 
     //Menu stuff
     $(".menuButton").click(function () {
@@ -53,4 +54,20 @@ $(function () {
                 }
             }
         });
+    $(function () {
+        $(".accordion").accordion();
+    });
+    $('.accordion').accordion({
+        active: false,
+        collapsible: true,
+        heightStyle: "content",
+    });
+
+    $(".accordion").click(function () {
+        console.log(active);
+        $("#folder" + active).switchClass("fa-folder-open", "fa-folder");
+        active = $(".accordion").accordion("option", "active");
+        console.log(active);
+        $("#folder" + active).switchClass("fa-folder", "fa-folder-open");
+    });
 });
